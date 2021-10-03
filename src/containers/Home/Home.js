ae070cb5b61b6619bd37aae6d302c17544ca2708
PH Saurav <phsaurav29@gmail.com>
2021-10-04 02:31:16 +0600

    Testing push4

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-5xl">This is the Home with tailwind Change</h1>
+			<h1 className="text-5xl">This is the Home with tailwind</h1>
 		</div>
 	);
 };
